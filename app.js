8a845bfda8e3b50402638821d3dcf82c70f4dda8
Munim_Ali <munimali52@gmail.com>
2022-06-18 21:46:44 +0500

    database connected

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-const express = require("express");
+const express = require("express")
 const colors = require('colors')
-const res = require("express/lib/response");
-const dotenv = require("dotenv").config();
+const res = require("express/lib/response")
+const dotenv = require("dotenv").config()
 const {errorHandler} = require('./backend/middleware/errorMiddleware')
-const connectDB = require('./backend/config/db');
-const port = process.env.PORT || 5000;
+const connectDB = require('./backend/config/db')
+const port = process.env.PORT || 5000
 
 const app = express();
 
-connectDB();
+connectDB()
 
-app.use(express.json());
-app.use(express.urlencoded({ extended : false }));
+app.use(express.json())
+app.use(express.urlencoded({ extended : false }))
 
-app.use('/api/users', require('./backend/routes/userRoutes'));
+app.use('/api/users', require('./backend/routes/userRoutes'))
 
 app.use(errorHandler)
 
 // console.log(`Server runs on ${port}`)
 app.listen(port, () => {
     console.log(`Server runs on ${port}`)
-});
+})
--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const connectDB = async ()=>{
     try{
-        const conn = await mongoose.connect("mongodb+srv://fariya:acme123@fariyalshusers.jsgkj.mongodb.net/?retryWrites=true&w=majority");
+        const conn = await mongoose.connect(process.env.MONGO_URI);
         console.log(`MONGODB Connected ${conn.connection.host}`.cyan.underline)
     } catch (error){
         console.log(error)
